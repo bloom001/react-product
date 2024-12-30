5fd345be2db0cb1990f9b6e9bed94a0c2595ccb9
bloom001 <shahzibahashraf627@gmail.com>
2024-12-30 15:33:44 +0530

    first react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductDetails from './componets/ProductsDetails';
+import Search from './componets/Search';
+import ShopCard from './componets/ShopCard';
+
+
+function App() {
+  return (
+    <div>
+      <ProductDetails />
+      <Search />
+      <ShopCard />
+
+    </div>
+  );
+}
+
+export default App;
